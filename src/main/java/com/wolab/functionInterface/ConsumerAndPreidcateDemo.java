133ad100a45e4010c1586dff8330e49cfabf72a9
xixiyayaheihei <sunkui324@sina.com>
2018-06-03 03:00:11 +0800

    add predicate functionInterface demo

---
+++ src/main/java/com/wolab/functionInterface/ConsumerAndPreidcateDemo.java
@@ -0,0 +1,44 @@
+package com.wolab.functionInterface;
+
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+/**
+ * 函数接口Consumer 例子
+ */
+public class ConsumerAndPreidcateDemo {
+
+
+    /**
+     *  Consumer 有且只有一个声明为accept(T t)的方法,接收一个输入参数,并且没有返回值.
+     */
+
+    /**
+     *  Predicate 有且只有一个声明为test(T t)的方法,并且返回一个boolean值.
+     */
+
+    public static Student updateStudentFee(Student student, Predicate<Student> predicate, Consumer<Student> consumer){
+
+        if(predicate.test(student)){
+            consumer.accept(student);
+        }
+
+        return  student;
+
+    }
+
+    public static void main(String[] args) {
+
+        Student student1=new Student("Ashok","Kumar",9.5);
+        student1=updateStudentFee(student1, student -> student.getGrade()>8.5, student -> student.feeDiscount = 30.0);
+        student1.printFee();
+
+        Student student2 =new Student("Jacke","Verma",8.0);
+        student2= updateStudentFee(student2,student -> student.getGrade()>=8.0,student -> student.feeDiscount = 20.0);
+        student2.printFee();
+
+    }
+
+
+
+}
---
+++ src/main/java/com/wolab/functionInterface/PredicateForList.java
@@ -0,0 +1,30 @@
+package com.wolab.functionInterface;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * Predicate 函数接口主要用于对输入的对象按照指定的条件进行过滤
+ */
+public class PredicateForList {
+    public static void main(String[] args) {
+        List<Integer> list= Arrays.asList(1,2,3,4,5,6,7,8,9);
+        System.out.println("输出所有参数:");
+        eval(list,n->true);
+        System.out.println("输出能被2整除的数字:");
+        eval(list,n->n%2==0);
+        System.out.println("输出不能2整除的数字:");
+        eval(list,n->n%2!=0);
+    }
+
+
+
+    public static void eval(List<Integer> list, Predicate<Integer> predicate){
+        for (Integer n:list){
+            if(predicate.test(n)){
+                System.out.print(n +" ");
+            }
+        }
+    }
+}
